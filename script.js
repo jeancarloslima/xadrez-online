ad09926dd34e7b13d84f0cffcb89ddd44ed6c0a5
Jean Carlos Oliveira Lima <jcoliveiralima19@gmail.com>
2025-05-31 15:23:21 -0300

    Adicionando sistema de movimento básico para os peões brancos

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const pecas = document.querySelectorAll(".img-peca");
 const pecasPretas = document.querySelectorAll(".peca-preta");
 const pecasBrancas = document.querySelectorAll(".peca-branca");
+const casasDisponiveis = document.querySelectorAll(".casa-disponivel");
 
 function verificaPeca(peca) {
     if (peca.classList.contains("img-peao")) {
@@ -11,23 +12,71 @@ function verificaPeca(peca) {
 function verificaCasasPeao(peca) {
     if (peca.classList.contains("peca-branca")) {
         if (peca.classList.contains("casa-inicial")) {
-            let coluna = peca.parentNode.classList[1];
-            let linha = peca.parentNode.parentNode;
+            
+            const coluna = peca.parentNode.classList[1];
+            const linha = peca.parentNode.parentNode;
 
-            let casaDisponivel1 = linha.previousElementSibling;
-            let elementoCasaDisponivel1 = casaDisponivel1.querySelector(`.${coluna} .casa-disponivel`);
-
-            let casaDisponivel2 = linha.previousElementSibling.previousElementSibling;
-            let elementoCasaDisponivel2 = casaDisponivel2.querySelector(`.${coluna} .casa-disponivel`);
+            const casaDisponivel1 = linha.previousElementSibling;
+            const elementoCasaDisponivel1 = casaDisponivel1.querySelector(`.${coluna} .casa-disponivel`);
+            
+            const casaDisponivel2 = linha.previousElementSibling.previousElementSibling;
+            const elementoCasaDisponivel2 = casaDisponivel2.querySelector(`.${coluna} .casa-disponivel`);
             
             elementoCasaDisponivel1.style.display = "block";
             elementoCasaDisponivel2.style.display = "block";
+
+            elementoCasaDisponivel1.addEventListener("click", () => {
+                movimentaPeca(peca, elementoCasaDisponivel1);
+            });
+            elementoCasaDisponivel2.addEventListener("click", () => {
+                movimentaPeca(peca, elementoCasaDisponivel2);
+            });
+        } else {
+            const coluna = peca.parentNode.classList[1];
+            const linha = peca.parentNode.parentNode;
+
+            const casaDisponivel1 = linha.previousElementSibling;
+            const elementoCasaDisponivel1 = casaDisponivel1.querySelector(`.${coluna} .casa-disponivel`);
+            
+            elementoCasaDisponivel1.style.display = "block";
+
+            elementoCasaDisponivel1.addEventListener("click", () => {
+                movimentaPeca(peca, elementoCasaDisponivel1);
+            });
         }
     }
 }
 
+function movimentaPeca(peca, casa) {
+    const colunaDisponivel = casa.parentNode;
+
+    peca.classList.remove("casa-inicial")
+
+    const novaPeca = document.createElement("i");
+    novaPeca.classList.add(peca.classList[0], peca.classList[1], peca.classList[2], peca.classList[3], peca.classList[4]);
+
+    colunaDisponivel.appendChild(novaPeca);
+    adicionaFuncoesAPeca(novaPeca);
+    peca.remove();
+    limpaCasasDisponiveis();
+}
+
+function adicionaFuncoesAPeca(peca) {
+    peca.addEventListener("click", () => {
+        limpaCasasDisponiveis();
+        verificaPeca(peca);
+    })
+}
+
+function limpaCasasDisponiveis() {
+    casasDisponiveis.forEach((casa) => {
+        casa.style.display = "none";
+    })
+}
+
 pecas.forEach(peca => {
     peca.addEventListener("click", () => {
+        limpaCasasDisponiveis();
         verificaPeca(peca);
     })
 });
