05d36a0a27a29a1fc92b861e20d1324d877f0eb0
Jean Carlos Oliveira Lima <jcoliveiralima19@gmail.com>
2025-06-01 01:54:53 -0300

    Problema de duplicação de peças resolvido

--- script.js
+++ script.js
@@ -12,37 +12,41 @@ function verificaPeca(peca) {
 function verificaCasasPeao(peca) {
     if (peca.classList.contains("peca-branca")) {
         if (peca.classList.contains("casa-inicial")) {
-            
             const coluna = peca.parentNode.classList[1];
             const linha = peca.parentNode.parentNode;
 
             const casaDisponivel1 = linha.previousElementSibling;
             const elementoCasaDisponivel1 = casaDisponivel1.querySelector(`.${coluna} .casa-disponivel`);
-            
+
             const casaDisponivel2 = linha.previousElementSibling.previousElementSibling;
             const elementoCasaDisponivel2 = casaDisponivel2.querySelector(`.${coluna} .casa-disponivel`);
-            
-            elementoCasaDisponivel1.style.display = "block";
-            elementoCasaDisponivel2.style.display = "block";
 
-            elementoCasaDisponivel1.addEventListener("click", () => {
-                movimentaPeca(peca, elementoCasaDisponivel1);
-            });
-            elementoCasaDisponivel2.addEventListener("click", () => {
-                movimentaPeca(peca, elementoCasaDisponivel2);
-            });
+            if (!elementoCasaDisponivel1.parentNode.querySelector("i") && !elementoCasaDisponivel2.parentNode.querySelector("i")) {
+                elementoCasaDisponivel1.style.display = "block";
+                elementoCasaDisponivel2.style.display = "block";
+
+                elementoCasaDisponivel1.addEventListener("click", () => {
+                    movimentaPeca(peca, elementoCasaDisponivel1);
+                });
+                elementoCasaDisponivel2.addEventListener("click", () => {
+                    movimentaPeca(peca, elementoCasaDisponivel2);
+                });
+            }
+
         } else {
             const coluna = peca.parentNode.classList[1];
             const linha = peca.parentNode.parentNode;
 
             const casaDisponivel1 = linha.previousElementSibling;
             const elementoCasaDisponivel1 = casaDisponivel1.querySelector(`.${coluna} .casa-disponivel`);
-            
-            elementoCasaDisponivel1.style.display = "block";
 
-            elementoCasaDisponivel1.addEventListener("click", () => {
-                movimentaPeca(peca, elementoCasaDisponivel1);
-            });
+            if (!elementoCasaDisponivel1.parentNode.querySelector("i")) {
+                elementoCasaDisponivel1.style.display = "block";
+
+                elementoCasaDisponivel1.addEventListener("click", () => {
+                    movimentaPeca(peca, elementoCasaDisponivel1);
+                });
+            }
         }
     }
 }
@@ -50,22 +54,10 @@ function verificaCasasPeao(peca) {
 function movimentaPeca(peca, casa) {
     const colunaDisponivel = casa.parentNode;
 
-    peca.classList.remove("casa-inicial")
+    peca.classList.remove("casa-inicial");
 
-    const novaPeca = document.createElement("i");
-    novaPeca.classList.add(peca.classList[0], peca.classList[1], peca.classList[2], peca.classList[3], peca.classList[4]);
-
-    colunaDisponivel.appendChild(novaPeca);
-    adicionaFuncoesAPeca(novaPeca);
-    peca.remove();
+    colunaDisponivel.appendChild(peca);
     limpaCasasDisponiveis();
-}
-
-function adicionaFuncoesAPeca(peca) {
-    peca.addEventListener("click", () => {
-        limpaCasasDisponiveis();
-        verificaPeca(peca);
-    })
 }
 
 function limpaCasasDisponiveis() {
